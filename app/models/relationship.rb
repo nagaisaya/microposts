1be2025104285700839d796f507f91414fb70707
sayanagai <saaypon@gmail.com>
2017-03-07 05:48:14 +0000

    add home view's timeline

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,8 +1,6 @@
 class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
-
-  validates :follower, presence: true
-  validates :followed, presence: true
-  validates :follower_id, uniqueness: { scope: :follower_id }
 end
+
+
