f43d5fe8517ebf262e71cef751b2b26dc75313f5
sayanagai <saaypon@gmail.com>
2017-03-07 11:55:22 +0000

    add heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228055126) do
+ActiveRecord::Schema.define(version: 20170307093920) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170228055126) do
     t.datetime "updated_at",      null: false
     t.string   "position"
     t.text     "s_introduction"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
